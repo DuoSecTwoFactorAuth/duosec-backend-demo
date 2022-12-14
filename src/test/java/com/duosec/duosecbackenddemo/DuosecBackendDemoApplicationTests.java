cfa896ad37707012037218a561ce4449a43503b8
Avinash Vijayvargiya <avinash14022002@gmail.com>
2022-12-14 23:44:04 +0530

    Initial Commit

---
+++ src/test/java/com/duosec/duosecbackenddemo/DuosecBackendDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duosec.duosecbackenddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuosecBackendDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
