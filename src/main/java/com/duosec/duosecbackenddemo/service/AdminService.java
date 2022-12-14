2264a17468644913cf7ab52e07a0fb4018baffc4
Avinash Vijayvargiya <avinash14022002@gmail.com>
2022-12-15 00:20:37 +0530

    Added: add employee

--- src/main/java/com/duosec/duosecbackenddemo/service/AdminService.java
+++ src/main/java/com/duosec/duosecbackenddemo/service/AdminService.java
@@ -1,8 +1,10 @@
 package com.duosec.duosecbackenddemo.service;
 
 import com.duosec.duosecbackenddemo.document.AdminDocument;
+import com.duosec.duosecbackenddemo.document.EmployeeDocument;
 import com.duosec.duosecbackenddemo.dto.Login;
 import com.duosec.duosecbackenddemo.repository.AdminRepository;
+import com.duosec.duosecbackenddemo.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,8 +20,16 @@ public class AdminService {
     @Autowired
     private AdminRepository adminRepository;
 
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
     public boolean login(Login login) {
         AdminDocument adminDocument = adminRepository.findByUsername(login.getUsername());
         return adminDocument.getPassword().equals(login.getPassword());
+    }
+
+    public boolean addEmployee(EmployeeDocument employeeDocument) {
+        employeeRepository.save(employeeDocument);
+        return true;
     }
 }
