999ce759529ed939b592a61fbad83813b4bf8839
Avinash Vijayvargiya <66490629+avinash14022002@users.noreply.github.com>
2022-12-15 00:32:14 +0530

    Added: employee login (#5)

---
+++ src/main/java/com/duosec/duosecbackenddemo/controller/EmployeeController.java
@@ -0,0 +1,33 @@
+package com.duosec.duosecbackenddemo.controller;
+
+import com.duosec.duosecbackenddemo.dto.Login;
+import com.duosec.duosecbackenddemo.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * User: Avinash Vijayvargiya
+ * Date: 15-Dec-22
+ * Time: 12:22 AM
+ */
+@RestController
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @PostMapping("/employee-login")
+    public ResponseEntity<Boolean> employeeLogin(@RequestBody Login login) {
+        try {
+            if (employeeService.login(login))
+                return new ResponseEntity<>(true, HttpStatus.ACCEPTED);
+            return new ResponseEntity<>(false, HttpStatus.UNAUTHORIZED);
+        } catch (Exception exception) {
+            return new ResponseEntity<>(false, HttpStatus.NOT_ACCEPTABLE);
+        }
+    }
+}
